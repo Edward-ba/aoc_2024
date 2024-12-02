d8be0c9913369645c4aa1cd2e782628052acd177
Edward-Ba <62915397+Edward-ba@users.noreply.github.com>
2024-12-01 22:28:29 -0800

    [choked] 3957/2925

---
+++ day_1_p2.py
@@ -0,0 +1,10 @@
+import re
+
+if __name__ == '__main__':
+    f = open('input1.txt', 'r').read().split('\n')
+    a = sorted([int(i.split('   ')[0]) for i in f])
+    b = sorted([int(i.split('   ')[1]) for i in f])
+    tot = 0
+    for i in a:
+        tot += b.count(i)*i
+    print(tot)
